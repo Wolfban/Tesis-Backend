fa40009348dc88a3ea19bb9a26249c42dfd9fc40
Esteban Alonso Barrera Araya <109553257+Wolfban@users.noreply.github.com>
2025-03-04 01:27:57 -0600

    Initial Commit

---
+++ src/main/java/com/example/tesisbackend/TesisBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.tesisbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesisBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesisBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tesisbackend/TesisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tesisbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesisBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
